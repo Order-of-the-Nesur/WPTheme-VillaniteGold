b16662ce56f98fe851ad189ecd4204974fefc047
Villanite <ariel@villanea.com>
2021-05-01 18:43:59 -0400

    Setting up Distribution folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,43 @@ var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var cleancss = require('gulp-clean-css');
 
-gulp.task('sass', function(cb) {
+
+gulp.task('themefiles', function (cb) {
+  gulp
+    .src(['index.php','functions.php','footer.php','header.php'])
+    .pipe(
+      gulp.dest('./dist')
+    );
+  cb();
+});
+
+gulp.task('images', function (cb) {
+  gulp
+    .src('images/*')
+    .pipe(
+      gulp.dest('./dist/images')
+    );
+  cb();
+});
+
+gulp.task('sass', function (cb) {
   gulp
     .src('sass/site.scss')
     .pipe(sass())
-    .pipe(cleancss({keepSpecialComments: "*"}))
-    .pipe(rename('style.css'))  
+    .pipe(cleancss({ keepSpecialComments: "*" }))
+    .pipe(rename('style.css'))
     .pipe(
-      gulp.dest('./')
+      gulp.dest('./dist')
     );
   cb();
 });
 
 gulp.task(
   'default',
-  gulp.series('sass', function(cb) {
+  gulp.series('sass', function (cb) {
     gulp.watch('sass/*.scss', gulp.series('sass'));
     cb();
   })
 );
+
+gulp.task('runwpbuild', gulp.parallel('themefiles', 'images', 'sass'), function(done){done();});
--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Welcome - Villanite</title>
+    <title><?php bloginfo('name'); wp_title(); ?></title>
     <link rel="preload" href="https://fonts.googleapis.com/css?family=Lora|Roboto&display=swap" as="style">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora|Roboto&display=swap">
     <?php wp_head(); ?>
@@ -14,11 +14,12 @@
     <header id="SiteHeader">
         <div id="MainNav">
             <a id="Logo" href="/">Villanite</a>
-            <div id="MenuOpener" onclick="toggleMenu(this)">
-                <div class="bar1"></div>
-                <div class="bar2"></div>
-                <div class="bar3"></div>
-            </div>            
+            <input type="checkbox">
+            <!-- <div id="MenuOpener" onclick="toggleMenu(this)"> -->
+                <span class="bar1"></span>
+                <span class="bar2"></span>
+                <span class="bar3"></span>
+            <!-- </div>             -->
             <nav id="SiteNav" role="navigation">
             <?php wp_nav_menu(array('theme_location' => 'header-menu')); ?>               
             </nav>
